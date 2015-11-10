3fc299d6d78d94b00a2e059d332bdf70dd41c7ee
João Marcus de Lemos <jmpierrotfernandes@hotmail.com>
2015-11-10 12:12:59 -0200

    Adicionado busca por matricula

--- inicio.c
+++ inicio.c
@@ -10,8 +10,11 @@ aluno * externo=nil;
 int main() {
   void iniciar_head();
   void inserir_aluno();
+  void inserir_materia(aluno * escolhido);
+  struct Saluno * buscar();
 
   int choice;
+  aluno * buscador;
 
   iniciar_head();
   while(true) {
@@ -22,6 +25,7 @@ int main() {
       printf("\n  2 - Inserir materia");
       printf("\n  3 - Inserir nota");
       printf("\n  4 - Mostrar todos");
+      printf("\n  5 - Encontrar");
     }
     printf("\n  0 - Sair");
     printf("\n>>");
@@ -33,13 +37,18 @@ int main() {
       inserir_aluno();
     }
     else if(choice==2 and externo!=nil) {
-
+      buscador = buscar();
+      inserir_materia(buscador);
     }
     else if(choice==3 and externo!=nil) {
-
+      buscador = buscar();
+      inserir_materia(buscador);
     }
     else if(choice==4 and externo!=nil) {
 
+    }
+    else if(choice==5 and externo!=nil) {
+      buscador = buscar();
     }
     else if(choice==0) {
       break;
@@ -72,12 +81,13 @@ void inserir_aluno() {
   scanf(" %[^\n]s%*c", name);
   printf("Diga os tres ultimos numeros da matricula: ");
   scanf("%i", &mat);
+  mat = 151047000 + (mat%1000);
 
   if(externo==nil) {
-    aux=malloc(sizeof(aluno));
+    aux=(struct Saluno *) malloc(sizeof(aluno));
     aux->proximo=(struct Sheader *) malloc(sizeof(head));
     aux->proximo=cabeca;
-    aux->matricula=151047000 + (mat%1000);
+    aux->matricula=mat;
     strcpy(aux->nome, name);
     aux->anterior=(struct Sheader *) malloc(sizeof(head));
     aux->anterior=cabeca;
@@ -88,7 +98,21 @@ void inserir_aluno() {
     externo=aux;
   }
   else {
+    aux=cabeca->proximo;
+
+    while(aux!=cabeca) {
+      if(mat == aux->matricula) {
+        printf("Matricula ja existente diga uma nova: ");
+        scanf("%i", &mat);
+        mat = 151047000 + (mat%1000);
+      }
+      else {
+        aux=aux->proximo;
+      }
+    }
+
     aux=cabeca->ultimo;
+
     if(aux->anterior==cabeca) {
       creat=malloc(sizeof(aluno));
       aux->proximo=creat;
@@ -98,7 +122,7 @@ void inserir_aluno() {
       creat->proximo=cabeca;
       cabeca->ultimo=creat;
       creat->turma=nil;
-      creat->matricula=151047000 + (mat%1000);
+      creat->matricula=mat;
       strcpy(creat->nome, name);
       aux->proximo=creat;
     }
@@ -111,23 +135,63 @@ void inserir_aluno() {
       creat->proximo=cabeca;
       cabeca->ultimo=creat;
       creat->turma=nil;
-      creat->matricula=151047000 + (mat%1000);
+      creat->matricula=mat;
       strcpy(creat->nome, name);
       aux->proximo=creat;
     }
     aux=aux->proximo;
   }
   cabeca->quant+=1;
-  printf("\nEspaco alocado: %p\nNome: %s\nMatricula: %i\n", aux, aux->nome, aux->matricula);
+
+  aux=cabeca->proximo;
+  while(aux->proximo!=cabeca) {
+    printf("\nEspaco alocado: %p - Matricula: %i - Nome: %s", aux, aux->matricula, aux->nome);
+    aux=aux->proximo;
+  }
+  aux=cabeca->ultimo;
+
+  printf("\n\nEspaco alocado: %p\nNome: %s\nMatricula: %i\n", aux, aux->nome, aux->matricula);
   printf("\nO aluno foi criado com sucesso deseja iniciar uma materia?\n1 - Sim\n0 - Nao\nResp: ");
   scanf("%d", &choice);
 
   switch (choice) {
     case 1:
-      //iniciar_materia(aux);
+      inserir_aluno(aux);
     case 0:
       printf("\n");
     default:
       printf("\nObrigado por usar");
   }
+}
+
+struct Saluno * buscar() {
+  int mat, choice;
+  char nome[NAMESIZE];
+  aluno * aux;
+
+  printf("\nDiga os três ultimos numeros da matricula: ");
+  scanf("%d", &mat);
+
+  mat = 151047000 + (mat%1000);
+  aux=externo;
+
+  while(aux!=cabeca) {
+    if(mat==aux->matricula) {
+      printf("\nEncontrado em:\n   Espaco alocado: %p - Matricula: %i - Nome: %s\n", aux, aux->matricula, aux->nome);
+      return aux;
+    }
+    aux=aux->proximo;
+  }
+
+  printf("\nNao encontrado, deseja inserir?\n1 - Sim\n0 - Nao\nResp: ");
+  scanf("%d", &choice);
+
+  if(choice==1) {
+    inserir_aluno();
+  }
+  return nil;
+}
+
+void inserir_materia(aluno * escolhido) {
+
 }
